b6fe24b37619d9fc6b0cbc339d3221fb9941fb95
Tim Reynolds <tsbach@gmail.com>
2021-01-29 21:27:24 +0800

    added svelte for quizzes

--- markdown_editor/app/svelte-site/src/content/peripherals/sub_perphs/testing.svelte
+++ markdown_editor/app/svelte-site/src/content/peripherals/sub_perphs/testing.svelte
@@ -1,1 +1,6 @@
-<p>this is testing</p>
+<script>
+  import Quiz from "@/Quiz.svelte";
+</script>
+
+<Quiz
+  questions={[{ question: 'Which is a duck?', answers: ['Mallard', 'Buffalo'], correct: [1, 0] }, { question: 'Which is a mammal?', answers: ['Mallard', 'Buffalo', 'Egret'], correct: [0, 1, 0] }, { question: 'Which is a person?', answers: ['Mallard', 'Harriet', 'Beatrice', 'Buffalo'], correct: [0, 1, 1, 0] }]} />
